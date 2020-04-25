11477f0243bd4ce639b854cb95d95af52a14ca8a
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-25 19:17:43 +0200

    Esercitazione Pari-Dispari Completata + Ottimizzazione Palindroma

--- palindroma/main.js
+++ palindroma/main.js
@@ -7,22 +7,52 @@ var parola_utente1 = prompt('Scrivi una parola'); // NB. il prompt mi fornisce u
 // 2.1 - Analizzare le singole lettere della parola inserita dall'utente partendo dalla posizione 0 fino alla posizione N.length
 // 2.1 - Analizzare le singole lettere della parola inserita dall'utente partendo dalla posizione N.length fino alla posizione 0
 
+// function func_palindroma() {
+//     var palindroma = '';
+//     for (var i = parola_utente1.length; i >= 0 ; i--) {
+//         var singole_lettere = parola_utente1.charAt(i);
+//         palindroma = palindroma + singole_lettere;
+//     }
+//     console.log('La tua parola scritta al contrario: ' + palindroma);
+//
+//     // 3.0 ------ Stampare un messaggio appropriato: ------
+//     // 3.1 - Se queste lettere sono uguali allora scrivi "Parola palindroma" (If ==)
+//     // 3.2 - Se queste lettere sono diverse allora scrivi "Parola NON palindroma" (Else !=)
+//     if (parola_utente1 == palindroma) {
+//         console.log('La parola inserita è palindroma');
+//     } else {
+//         console.log('La parola inserita NON è palindroma');
+//     }
+// }
+//
+// func_palindroma();
+
+
 function func_palindroma() {
     var palindroma = '';
     for (var i = parola_utente1.length; i >= 0 ; i--) {
         var singole_lettere = parola_utente1.charAt(i);
         palindroma = palindroma + singole_lettere;
     }
-    console.log('La tua parola scritta al contrario: ' + palindroma);
 
     // 3.0 ------ Stampare un messaggio appropriato: ------
     // 3.1 - Se queste lettere sono uguali allora scrivi "Parola palindroma" (If ==)
     // 3.2 - Se queste lettere sono diverse allora scrivi "Parola NON palindroma" (Else !=)
     if (parola_utente1 == palindroma) {
-        console.log('La parola inserita è palindroma');
+        return ('La parola ' + '\"' + parola_utente1 + '\"' + ' da te inserita è palindroma');
     } else {
-        console.log('La parola inserita NON è palindroma');
+        return ('La parola ' + '\"' + parola_utente1 + '\"' + ' da te inserita NON è palindroma. ' + '('+ palindroma +')');
     }
 }
 
-func_palindroma();
+var risultato = func_palindroma();
+console.log(risultato);
+
+
+
+
+
+
+
+
+// END
--- pari-dispari/main.js
+++ pari-dispari/main.js
@@ -1,1 +1,34 @@
 // gioco pari o dispari contro il pc: chiedere all'utente un numero tra 1 e 5 e generare un numero random tra 1 e 5. Chiedere all'utente se sceglie pari o dispari, quindi fare la somma dei due numeri (giocata utente + giocata pc), quindi valutare se il risultato è pari o dispari e comunicare all'utente se ha vinto o meno
+
+// 1 - Chiedere all'utente un numero tra 1 e 5
+var numero_utente = parseInt(prompt('Digita un numero da 1 a 5'));
+console.log('L\'utente ha digitato il seguente numero: ' + numero_utente);
+
+// 2 - Generare un numero random tra 1 e 5
+function generaRandom (min, max) {
+    var numero_random = Math.floor(Math.random() * (max - min + 1)) + min;
+    return numero_random;
+}
+var random_pc = generaRandom(1, 5);
+console.log('Il PC ha generato il seguente numero: ' + random_pc);
+
+// 4 - Fare la somma dei due numeri (giocata utente + giocata pc)
+var somma = random_pc + numero_utente;
+console.log('La somma tra il numero scelto dall\'utente ' + numero_utente + ' e il numero ' + random_pc + ' generato dal PC è: ' + somma);
+
+// 3 - Chiedere all'utente se sceglie pari o dispari
+var pari_dispari = prompt('Scegli Pari o Dispari e scrivilo qui sotto');
+console.log('L\'utente ha scelto: ' + pari_dispari);
+
+// 5 - Valutare se il risultato è pari o dispari
+// 6 - Comunicare all'utente se ha vinto o meno
+if ((somma % 2 == 0) && (pari_dispari == 'pari')) {
+    alert('Complimenti hai vinto! Prova di nuovo aggiornando la pagina.');
+    console.log('Complimenti hai vinto! Prova di nuovo aggiornando la pagina.');
+} else if ((somma % 2 != 0) && (pari_dispari == 'dispari')) {
+    alert('Complimenti hai vinto! Prova di nuovo aggiornando la pagina.');
+    console.log('Complimenti hai vinto! Prova di nuovo aggiornando la pagina.');
+} else {
+    alert('Hai perso! Prova di nuovo aggiornando la pagina.');
+    console.log('Hai perso! Prova di nuovo aggiornando la pagina.');
+}
